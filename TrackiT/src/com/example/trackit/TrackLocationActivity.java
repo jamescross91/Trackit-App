b5e07154e119d0c5ae76a085f802fe1218e97a43
James Cross <james@usethatspace.com>
2014-12-23 23:17:32 +0000

    Imported from SVN by Bitbucket

---
+++ TrackiT/src/com/example/trackit/TrackLocationActivity.java
@@ -0,0 +1,156 @@
+package com.example.trackit;
+
+import java.util.Date;
+import java.util.List;
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.Window;
+import android.widget.Toast;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+
+public class TrackLocationActivity extends MapActivity {
+
+	protected Drawable drawable;
+	protected MapOverlay itemizedOverlay;
+	protected List<Overlay> mapOverlays;
+	protected MapView mapView;
+	private MapActivity thisActivity;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_LEFT_ICON);
+
+		setContentView(R.layout.activity_track_location);
+
+		getWindow().setFeatureDrawableResource(Window.FEATURE_LEFT_ICON,
+				R.drawable.icon);
+
+		mapView = (MapView) findViewById(R.id.mapview);
+		mapView.setBuiltInZoomControls(true);
+
+		mapOverlays = mapView.getOverlays();
+
+		drawable = this.getResources().getDrawable(R.drawable.marker);
+		itemizedOverlay = new MapOverlay(drawable, this);
+
+		registerReceiver(receiver, new IntentFilter("TrackiTLoc"));
+		registerReceiver(receiver, new IntentFilter("DeviceDelete"));
+		thisActivity = this;
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (receiver != null) {
+			unregisterReceiver(receiver);
+			receiver = null;
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (receiver != null) {
+			unregisterReceiver(receiver);
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		registerReceiver(receiver, new IntentFilter("TrackiTLoc"));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_track_location, menu);
+		return true;
+	}
+
+	@Override
+	public void onBackPressed() {
+
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		return true;
+	}
+	
+	private BroadcastReceiver receiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			if (intent.getAction().equals("TrackiTLoc")) {
+
+				mapOverlays.clear();
+				itemizedOverlay.clearOverlays();
+				double lng = intent.getExtras().getDouble("GEO_LONG");
+				double lat = intent.getExtras().getDouble("GEO_LAT");
+
+				GeoPoint point = new GeoPoint((int) (lat * 1E6),
+						(int) (lng * 1E6));
+
+				Date d = new Date();
+				CharSequence s = DateFormat.format(
+						"kk:mm on EEEE, MMMM d, yyyy ", d.getTime());
+
+				OverlayItem overlayitem = new OverlayItem(point,
+						"TrackiT Location", "Sent to server at: " + s);
+
+				itemizedOverlay.addOverlay(overlayitem);
+				mapOverlays.add(itemizedOverlay);
+
+				mapView.invalidate();
+
+				Toast.makeText(context, "New location added to the map",
+						Toast.LENGTH_LONG).show();
+			}
+
+			if (intent.getAction().compareTo(
+					context.getString(R.string.device_delete_broadcast_action)) == 0) {
+
+				SharedPreferences prefs = context.getSharedPreferences(
+						context.getString(R.string.authentication), 0);
+				SharedPreferences.Editor editor = prefs.edit();
+				editor.remove("authToken");
+				editor.remove("device_id");
+				editor.remove("authenticated");
+				editor.commit();
+				
+				Intent serviceIntent = new Intent(context,
+						LocationService.class);
+				stopService(serviceIntent);
+				
+				Intent serviceIntent2 = new Intent(context,
+						com.commonsware.cwac.locpoll.LocationPollerService.class);
+				stopService(serviceIntent2);
+
+				Intent loginIntent = new Intent(context, MainActivity.class);
+				startActivity(loginIntent);
+			}
+		}
+	};
+
+	@Override
+	protected boolean isRouteDisplayed() {
+
+		return false;
+	}
+}
---
+++ TrackiT/src/com/example/trackit/TrackiTLocation.java
@@ -0,0 +1,90 @@
+package com.example.trackit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.util.Log;
+
+public class TrackiTLocation extends Network {
+
+	private Location location;
+	private double latitude;
+	private double longitude;
+	private double accuracy;
+	private double altitude; 
+	private double bearing; 
+	private double speed;
+	private String authToken;
+	
+	public TrackiTLocation(Context context, Location location) {
+		super(context);
+		this.location = location;
+		latitude = this.location.getLatitude();
+		longitude = this.location.getLongitude();
+		accuracy = this.location.getAccuracy();
+		altitude = this.location.getAltitude();
+		bearing = this.location.getBearing();
+		speed = this.location.getSpeed();
+		SharedPreferences auth = context.getSharedPreferences(
+				context.getString(R.string.authentication), 0);
+		 authToken = auth.getString("authToken", "");
+	}
+	
+	public JSONObject toJSON(){
+		JSONObject object = new JSONObject();
+		try {
+			object.put("latitude", latitude);
+			object.put("longitude", longitude);
+			object.put("accuracy", accuracy);
+			object.put("altitude", altitude);
+			object.put("bearing", bearing);
+			object.put("speed", speed);
+		} catch (JSONException e) {
+			Log.e("JSON Conversion", e.getMessage());
+		}
+		
+		return object;
+	}
+	
+	private void persistLocation(){
+		
+		SharedPreferences auth = thisContext.getSharedPreferences(
+				thisContext.getString(R.string.authentication), 0);
+		String deviceID = auth.getString("device_id", "");
+		
+		List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+		pairs.add(new BasicNameValuePair("latitude", String.valueOf(latitude)));
+		pairs.add(new BasicNameValuePair("longitude", String.valueOf(longitude)));
+		pairs.add(new BasicNameValuePair("accuracy", String.valueOf(accuracy)));
+		pairs.add(new BasicNameValuePair("altitude", String.valueOf(altitude)));
+		pairs.add(new BasicNameValuePair("bearing", String.valueOf(bearing)));
+		pairs.add(new BasicNameValuePair("speed", String.valueOf(speed)));		
+		pairs.add(new BasicNameValuePair("deviceID", deviceID));
+		pairs.add(new BasicNameValuePair("authToken", authToken));
+		
+		networkExec(formatLocUrl(), pairs);
+	}
+	
+	private String formatLocUrl(){
+		String URL = thisContext.getString(R.string.server_root)
+				+ thisContext.getString(R.string.location_url);
+
+		return URL;
+	}
+	
+	protected String doInBackground(String... params) {
+		if(!isConnected())
+			return null;
+		
+		persistLocation();
+		return null;
+	}
+}
